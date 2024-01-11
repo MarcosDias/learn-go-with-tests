9ca19bb25a3174ff22bad233868f0a1f7ed677b4
Marcos Dias <marcos.a.melo.dias@gmail.com>
2024-01-11 11:21:16 -0300

    Hello World, now in new language

--- hello.go
+++ hello.go
@@ -2,15 +2,22 @@ package main
 
 import "fmt"
 
+const SPANISH = "Spanish"
 const ENGLISH_HELLO_PREFIX = "Hello, "
+const SPANISH_HELLO_PREFIX = "Hola, "
 
-func Hello(name string) string {
+func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
 	}
+
+	if language == SPANISH {
+		return SPANISH_HELLO_PREFIX + name
+	}
+
 	return ENGLISH_HELLO_PREFIX + name
 }
 
 func main() {
-	fmt.Println(Hello(""))
+	fmt.Println(Hello("", ""))
 }
