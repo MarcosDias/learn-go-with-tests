4ca9fcc3ce811b9d45a3d6a58891ec3cd8e32294
Marcos Dias <marcos.a.melo.dias@gmail.com>
2024-01-12 14:02:57 -0300

    Practice exercises - add option to many times the character will repeat and use the string package

--- 3_Iteration/repeat.go
+++ 3_Iteration/repeat.go
@@ -1,11 +1,9 @@
 package iteration
 
+import "strings"
+
 const REPEAT_COUNT = 5
 
-func Repeat(character string) string {
-	var repeated string
-	for i := 0; i < REPEAT_COUNT; i++ {
-		repeated += character
-	}
-	return repeated
+func Repeat(character string, count int) string {
+	return strings.Repeat(character, count)
 }
--- 3_Iteration/repeat_test.go
+++ 3_Iteration/repeat_test.go
@@ -1,18 +1,27 @@
 package iteration
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestRepeat(t *testing.T) {
-	repeat := Repeat("a")
-	expected := "aaaaa"
+	repeat := Repeat("a", 3)
+	expected := "aaa"
 
 	if repeat != expected {
 		t.Errorf("expected %q but got %q", expected, repeat)
 	}
 }
 
+func ExampleRepeat() {
+	repeat := Repeat("b", 3)
+	fmt.Println(repeat)
+	// Output: bbb
+}
+
 func BenchmarkRepeat(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		Repeat(("a"))
+		Repeat("a", 5)
 	}
 }
