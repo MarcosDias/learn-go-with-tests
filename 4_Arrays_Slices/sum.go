546a40d35138d492ab935b0a3debb4ca9a275f5f
Marcos Dias <marcos.a.melo.dias@gmail.com>
2024-01-13 10:24:25 -0300

    Finished modules Arrays and Slices

--- 4_Arrays_Slices/sum.go
+++ 4_Arrays_Slices/sum.go
@@ -6,3 +6,29 @@ func Sum(numbers []int) (sum int) {
 	}
 	return
 }
+
+func SumAll(numbersToSum ...[]int) []int {
+	// lengthOfNumbers := len(numbersToSum)
+	// sums := make([]int, lengthOfNumbers)
+	var sums []int
+
+	for _, numbers := range numbersToSum {
+		// sums[i] = Sum(numbers)
+		sums = append(sums, Sum(numbers))
+		// append
+		// Return a new slice in a new size
+	}
+	return sums
+}
+
+func SumAllTails(numbersToSim ...[]int) (sums []int) {
+	for _, numbers := range numbersToSim {
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+			tail := numbers[1:]
+			sums = append(sums, Sum(tail))
+		}
+	}
+	return
+}
--- 4_Arrays_Slices/sum_test.go
+++ 4_Arrays_Slices/sum_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"slices"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
 	t.Run("Collection of 5 numbers", func(t *testing.T) {
@@ -17,3 +20,35 @@ func TestSum(t *testing.T) {
 // Coverage tool:
 // https://go.dev/blog/cover
 // go test -cover
+
+func TestSumAll(t *testing.T) {
+	got := SumAll([]int{1, 2}, []int{0, 9})
+	want := []int{3, 9}
+
+	// Before go 1.21
+	// !reflect.DeepEqual(got, want)
+	if !slices.Equal(got, want) {
+		t.Errorf("got %v want %v", got, want)
+	}
+}
+
+func TestSumAllTails(t *testing.T) {
+	checkSums := func(t testing.TB, got, want []int) {
+		t.Helper()
+		if !slices.Equal(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+	}
+	t.Run("make the sums of some slices", func(t *testing.T) {
+		got := SumAllTails([]int{1, 2}, []int{0, 9})
+		want := []int{2, 9}
+
+		checkSums(t, got, want)
+	})
+	t.Run("safely sum empty slices", func(t *testing.T) {
+		got := SumAllTails([]int{}, []int{3, 4, 5})
+		want := []int{0, 9}
+
+		checkSums(t, got, want)
+	})
+}
