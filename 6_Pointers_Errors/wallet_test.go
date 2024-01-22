4d3821ac2938a646abb8733b00587d84e301af6c
Marcos Dias <marcos.a.melo.dias@gmail.com>
2024-01-22 00:53:04 -0300

    Added lint go and update code to cover every cases

--- 6_Pointers_Errors/wallet_test.go
+++ 6_Pointers_Errors/wallet_test.go
@@ -21,14 +21,25 @@ func TestWallet(t *testing.T) {
 			t.Errorf("got %q, want %q", got, want)
 		}
 	}
+
+	assertNoError := func(t testing.TB, got error) {
+		t.Helper()
+		if got != nil {
+			t.Fatal("got an error but didn't want one")
+		}
+	}
 	t.Run("deposit", func(t *testing.T) {
 		wallet := Wallet{}
 		wallet.Deposit(Bitcoin(10))
 		assertBalance(t, wallet, Bitcoin(10))
 	})
-	t.Run("withdraw", func(t *testing.T) {
+	t.Run("withdraw with funds", func(t *testing.T) {
 		wallet := Wallet{balance: Bitcoin(20)}
-		wallet.Withdraw(Bitcoin(10))
+		err := wallet.Withdraw(Bitcoin(10))
+		// The line below was added after of
+		// go install github.com/kisielk/errcheck@latest
+		// errcheck .
+		assertNoError(t, err)
 		assertBalance(t, wallet, Bitcoin(10))
 	})
 	t.Run("withdraw insufficient funds", func(t *testing.T) {
