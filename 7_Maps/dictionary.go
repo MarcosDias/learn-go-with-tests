145c7ff8403b2989caffaec4cf13a08c6069f51c
Marcos Dias <marcos.a.melo.dias@gmail.com>
2024-01-23 01:14:24 -0300

    Added function to add on Dictionary

--- 7_Maps/dictionary.go
+++ 7_Maps/dictionary.go
@@ -1,10 +1,17 @@
 package main
 
-import "errors"
+const (
+	ErrNotFound   = DictionaryErr("could not find the word you were looking for")
+	ErrWordExists = DictionaryErr("cannot add word because it already exists")
+)
+
+type DictionaryErr string
+
+func (e DictionaryErr) Error() string {
+	return string(e)
+}
 
 type Dictionary map[string]string
-
-var ErrNotFound = errors.New("could not find the word you were looking for")
 
 func (d Dictionary) Search(word string) (string, error) {
 	definition, ok := d[word]
@@ -12,4 +19,21 @@ func (d Dictionary) Search(word string) (string, error) {
 		return "", ErrNotFound
 	}
 	return definition, nil
+}
+
+func (d Dictionary) Add(word, definition string) error {
+	_, err := d.Search(word)
+	switch err {
+	case ErrNotFound:
+		d[word] = definition
+	case nil:
+		return ErrWordExists
+	default:
+		return err
+	}
+
+	return nil
+
+	d[word] = definition
+	return nil
 }
