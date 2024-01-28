bd8a7a033c321323b0c029c3eb5f421e997f9661
Marcos Dias <marcos.a.melo.dias@gmail.com>
2024-01-28 01:53:48 -0300

    Start using dependency injection with Go. Rewriting fmt.Printf

---
+++ 8_Dependency_Injection/greet_test.go
@@ -0,0 +1,18 @@
+package dependencyinjection
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestGreet(t *testing.T) {
+	buffer := bytes.Buffer{}
+	Greet(&buffer, "Marcos")
+
+	got := buffer.String()
+	want := "Hello, Marcos"
+
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
