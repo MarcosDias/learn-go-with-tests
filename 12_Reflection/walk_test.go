d2b35f664a0101532670c26a95284ac17df9df1f
Marcos Dias <marcos.a.melo.dias@gmail.com>
2024-02-27 14:08:05 -0300

    New case test table

--- 12_Reflection/walk_test.go
+++ 12_Reflection/walk_test.go
@@ -1,20 +1,43 @@
 package walk
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestWalk(t *testing.T) {
-	expected := "Chris"
-	var got []string
+	cases := []struct {
+		Name          string
+		Input         interface{}
+		ExpectedCalls []string
+	}{
+		{
+			"struct with one string field",
+			struct {
+				Name string
+			}{"Marcos"},
+			[]string{"Marcos"},
+		},
+		{
+			"struct with two string field",
+			struct {
+				Name    string
+				Country string
+			}{"Marcos", "Brasil"},
+			[]string{"Marcos", "Brasil"},
+		},
+	}
 
-	x := struct {
-		Nam string
-	}{expected}
+	for _, test := range cases {
+		t.Run(test.Name, func(t *testing.T) {
+			var got []string
+			walk(test.Input, func(input string) {
+				got = append(got, input)
+			})
 
-	walk(x, func(input string) {
-		got = append(got, input)
-	})
-
-	if got[0] != expected {
-		t.Errorf("got %q, want %q", got[0], expected)
+			if !reflect.DeepEqual(got, test.ExpectedCalls) {
+				t.Errorf("got %v, want %v", got, test.ExpectedCalls)
+			}
+		})
 	}
 }
